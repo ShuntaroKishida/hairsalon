8742e52b5c7109124abdcdc9c4164a21810fa176
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-08-30 13:43:19 +0900

    userモデルの単体テスト実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nameが空では登録できない' do
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Name can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end  
+  end
+
+end
+
+# bundle exec rspec spec/models/user_spec.rb
